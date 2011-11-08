b0015e0829dcd7421a361a63237e6a4d2211f636
pavuk <pavuk@8mag.net>
2011-11-08 15:51:35 +0100

    Signed-off-by: pavuk <pavuk@8mag.net>

---
+++ test.py
@@ -0,0 +1,12 @@
+# test1
+import web
+
+urls = ("/.*", "hello")
+app = web.application(urls, globals())
+
+class hello:
+    def GET(self):
+        return 'Hello, world!'
+
+if __name__ == "__main__":
+    app.run()
